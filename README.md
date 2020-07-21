48aaf52da2597d9adad1aa7292f7d9a9d41a6c9e
Jamil Bashir <jamil@themindgauge.com>
2020-07-21 18:54:26 +0500

    Basic files for plugin

---
+++ README.md
@@ -0,0 +1,2 @@
+# Basic React Files for WordPress
+The repository contains the very basic Required files to create a react app with JSX (in WordPress).
---
+++ aragon-connect-plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Aragon Connect
+ * Plugin URI: 
+ * Description: Aragon Connect.
+ * Version: 1.0
+ * Author: Jamil Bashir
+ * Author URI: 
+ */
+/**
+ * WordPress Setting Page for a React.
+ *
+*/
+if(!define('WPINC')){
+	
+}
